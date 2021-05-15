6d5dbc4f6f0e055d4bb06f367115ba88dfa2603f
Muntaha Ibn Ahmed <muntaha.techlogicians@gmail.com>
2021-05-15 20:49:56 +0600

    Page not found modified

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return ( <div className="">
-        404 page Not Found
-    </div> );
-}
- 
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2 className="">
+        404
+        <br/>
+        Page Not Found!
+      </h2>
+
+      <Link to={`/`} className={`btn`}>
+        Home
+      </Link>
+    </div>
+  );
+};
+
 export default NotFound;
